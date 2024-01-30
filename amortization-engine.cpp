6d25457bedf21b71ed53479a3b8b92f6e0dd130b
Devansh Garg <147437567+ThePredictiveDev@users.noreply.github.com>
2024-01-30 17:12:22 +0530

    Improved Output

--- amortization-engine.cpp
+++ amortization-engine.cpp
@@ -3,6 +3,42 @@
 #include <iomanip>
 using namespace std;
 
+enum AmortEnum{
+    YR_OUT = 2,
+    MN_OUT = 2,
+    CUM_MN_OUT = 3,
+    PAYMENT_OUT = 12,
+    INTEREST_OUT = 12,
+    PRINCIPAL_OUT = 12,
+    BALANCE_OUT = 16
+
+};
+
+string AmortHeader[]{
+    "Yr",
+    "Mn",
+    "CumMn",
+    "Payment",
+    "Interest",
+    "Principal",
+    "Balance"
+
+};
+
+void printAmortHeader(){
+    cout<<
+        endl << 
+        setw(YR_OUT) << AmortHeader[0] << " " <<
+        setw(MN_OUT) << AmortHeader[1] << " " <<
+        setw(CUM_MN_OUT) << AmortHeader[2] << "" <<
+        setw(PAYMENT_OUT) << AmortHeader[3] << "" <<
+        setw(INTEREST_OUT) << AmortHeader[4] << " " <<
+        setw(PRINCIPAL_OUT) << AmortHeader[5] << " " <<
+        setw(BALANCE_OUT) << AmortHeader[6] << 
+        endl<<
+        endl;
+
+}
 
 int main(){
 
@@ -33,21 +69,45 @@ int main(){
     double currPrincipalPayment = 0.0;
     double currBalance = 0.0;
     long currentLoanMonth = 1;
-
+    int year = 1;
+    int switchYear = 0;
+    int yearMonth = 1;
     currBalance = principal;
 
     while(currentLoanMonth<=monthsOfLoan){
+
+        if(switchYear != year){
+            switchYear = year;
+            printAmortHeader();
+        }
+
         currInterestPayment = currBalance*monthInterest;
         currPrincipalPayment= payment - currInterestPayment;
         currBalance = currBalance - currPrincipalPayment;
 
-        cout<< "CM: " << currentLoanMonth << " CI:  "<< currInterestPayment << " CP: " << currPrincipalPayment << " Bal: " << currBalance<< endl;
+        cout<< 
+            setw(YR_OUT) << year << " " <<
+            setw(MN_OUT) << yearMonth << " " <<
+            setw(CUM_MN_OUT) << currentLoanMonth << " " <<
+            setw(PAYMENT_OUT) << payment << " " <<
+            setw(INTEREST_OUT) << currInterestPayment << " " <<
+            setw(PRINCIPAL_OUT) << currPrincipalPayment << " " <<
+            setw(BALANCE_OUT) << abs(currBalance) << endl;
+
+        yearMonth++;
+
+        if(yearMonth>12){
+            yearMonth = 1;
+            year++;
+        }
 
         currentLoanMonth++;
 
     }
 
-    cout << "loan payments complete" << endl;
+    cout << endl <<
+     "loan payments complete" <<
+      endl;
 
     return 0;
 }
